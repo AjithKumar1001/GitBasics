22b8e0a1cf00037729efc605d48e1e4b2121a05e
ajith <ajithkumar10012000@gmail.com>
2024-12-02 21:42:57 +0530

    First commit

---
+++ src/test/java/screenShot/practice.java
@@ -0,0 +1,39 @@
+package screenShot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class practice {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "I:\\Seleniumdriver\\chromedriver-win64\\chromedriver-win64");
+
+		driver.get("http:www.google.com");
+
+		practice p = new practice();
+		p.screenUtility(driver, "Homepage");
+
+	}
+
+	public static void screenUtility(WebDriver driver, String ScreenshotName) throws IOException {
+		// TODO Auto-generated method stub
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File srcFile = ts.getScreenshotAs(OutputType.FILE);
+		File destination = new File("./Screenshots/" + ScreenshotName + ".png");
+		FileUtils.copyFile(srcFile, destination);
+
+	}
+
+}
---
+++ src/test/java/screenShot/practise1.java
@@ -0,0 +1,26 @@
+package screenShot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practise1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		WebElement email = driver.findElement(By.name("email"));
+		File screeshot = email.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screeshot, new File("./screenshot/element.png"));
+
+	}
+
+}
