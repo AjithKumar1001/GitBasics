0852ce4b73510d952b28a22adc2bde7c30010c74
ajith <ajithkumar10012000@gmail.com>
2024-12-03 13:30:46 +0530

    Final commit

---
+++ src/test/java/screenShot/Jtest.java
@@ -0,0 +1,25 @@
+package screenShot;
+
+public class Jtest {
+	public static void main(String[] args) {
+		
+		printer();
+		printers();
+		
+	}
+	
+	public static void printer()
+	{
+		System.out.println("This is changed code");
+		System.out.println("This is changed Workspace");
+		System.out.println("This is changed Eclipse");
+	}
+	
+	public static void printers()
+	{
+		System.out.println("This is changed Benjamnin");
+		System.out.println("This is changed Edward");
+		System.out.println("This is changed Eclipse");
+	}
+
+}
